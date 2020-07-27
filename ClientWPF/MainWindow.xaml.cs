daebf2c860a6c32be9efd12250681a443099a629
Kiryjm <kiryjm@gmail.com>
2020-07-27 04:33:24 +0300

    Adding empty WPF Client project

---
+++ ClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            Card project = await GetAPIAsync("https://localhost:44356/api/cards/1");
+        }
+    }
+}
